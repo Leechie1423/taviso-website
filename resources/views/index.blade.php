f7408b48b32b97f167a102c8e87518b2599240c9
Steve Leech <steve.leech1@gmail.com>
2026-03-20 17:17:54 +0000

    Added favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -231,7 +231,7 @@
 						d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
 				</svg>
-				Address 1, Pavoits Street, Woodbridge, UK
+
 			</div>
 			<div class="flex items-center gap-2 text-gray-500">
 				<svg class="h-5 w-5 text-gray-800" fill="currentColor" viewBox="0 0 24 24">
